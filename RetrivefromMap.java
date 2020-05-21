92de178c88171d54a4abf9f1f10b836f5e6d024e
arthi2206 <60561674+arthi2206@users.noreply.github.com>
2020-05-21 10:52:24 +0530

    Update RetrivefromMap.java

--- RetrivefromMap.java
+++ RetrivefromMap.java
@@ -8,8 +8,7 @@ public class RetrivefromMap {
 	public static void main(String[] args) {
 //		 Write a Java Program to print the keys and values from the Map
 //		 map = {A=1, B=2, C=3, D=4, E=5}
-//		 (Min 3 ways)
-//		 Note: Even if you google understand what you are doing!
+
 
 		Map<Character,Integer> m=new HashMap<Character,Integer>();
 		m.put('A',1);
